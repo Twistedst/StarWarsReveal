6436f0e1d887be98b62b16cbebb6b5167d375bc6
Steven Hawley <hawleyst@mail.gvsu.edu>
2018-11-07 22:08:18 -0500

    Reset the stars and regenerate on resize

--- src/js/main.js
+++ src/js/main.js
@@ -2,17 +2,26 @@
 const numStars = 1000;
 
 // For every star we want to display
-for (let i = 0; i < numStars; i++) {
-  let star        = document.createElement('div');
-  star.className  = 'star';
-  star.id         = 'star' + i;
-  var xy          = getRandomPosition();
-  star.style.top  = xy[0] + 'px';
-  star.style.left = xy[1] + 'px';
-  document.body.append(star);
-  setInterval(function () {
-    flashtext('star' + i, 'yellow');
-  }, (1000 % i) * 2);
+function generateStars() {
+  for (let i = 0; i < numStars; i++) {
+    let star        = document.createElement('div');
+    star.className  = 'star';
+    star.id         = 'star' + i;
+    var xy          = getRandomPosition();
+    star.style.top  = xy[0] + 'px';
+    star.style.left = xy[1] + 'px';
+    document.body.append(star);
+    setInterval(function () {
+      flashtext('star' + i, 'yellow');
+    }, (1000 % i) * 2);
+  }
+}
+
+function removeStars(){
+  for(let i = 0; i < numStars; i++){
+    let star = document.getElementById('star' + i);
+    star.parentNode.removeChild(star);
+  }
 }
 
 // Gets random x, y values based on the size of the container
@@ -36,3 +45,12 @@ function flashtext(ele, col) {
     document.getElementById(ele).style.backgroundColor = 'white';
   }
 }
+
+window.onload   = function () {
+  generateStars();
+};
+
+window.onresize = function () {
+  removeStars();
+  generateStars();
+};
